4aa653ae86df22542290b962dff1514fc95b77ac
imran9007 <imran9007@users.noreply.github.com>
2023-02-04 15:01:20 +0530

    javascript done

--- App.js
+++
@@ -1,6 +0,0 @@
-console.log('script is working from a different file')
-
-var username = prompt ('give me username')
-var welcomemessage = 'the script is working '+ username
-
-alert(welcomemessage)
