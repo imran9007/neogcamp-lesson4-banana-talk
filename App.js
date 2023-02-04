fec04cafecad8e89066f6866af5346d6ed074ac3
imran9007 <imran9007@users.noreply.github.com>
2023-02-04 14:57:41 +0530

    new js file, alert and prompt

---
+++ App.js
@@ -0,0 +1,6 @@
+console.log('script is working from a different file')
+
+var username = prompt ('give me username')
+var welcomemessage = 'the script is working '+ username
+
+alert(welcomemessage)
