bb5c791b216f0b2071855188df918cf9c5647e1a
imran9007 <imran9007@users.noreply.github.com>
2023-02-04 18:21:22 +0530

    input, process output ready

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 console.log(txtinput);
 
+var outputdiv = document.querySelector("#output");
+
+outputdiv.innerText = "i am imran"
+
 
 function clickHandler() {
     console.log('clicked')
