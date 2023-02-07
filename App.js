899e68464ea9ec5e1a8fec82948744f6d0a9c4d3
imran9007 <imran9007@users.noreply.github.com>
2023-02-07 19:20:16 +0530

    buttons

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 var btnTranslate = document.querySelector("#btn-translate");
 
 var txtinput = document.querySelector("#txt-input");
-console.log(txtinput);
 
 var outputdiv = document.querySelector("#output");
 
